9513d9f41da403fa38801cd35b74da51025f89a0
Arun Nampally <arun.nampally@invitae.com>
2020-12-23 18:07:36 -0800

    cleanup

--- init.el
+++ init.el
@@ -5,8 +5,3 @@
 (unless package--initialized (package-initialize))
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
-
-; @begin(68483553)@ - Do not edit these lines - added automatically!
-(if (file-exists-p "/Users/arun.nampally/.ciaoroot/master/ciao_emacs/elisp/ciao-site-file.el")
-  (load-file "/Users/arun.nampally/.ciaoroot/master/ciao_emacs/elisp/ciao-site-file.el"))
-; @end(68483553)@ - End of automatically added lines.
