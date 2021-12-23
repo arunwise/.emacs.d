0eb5b420e8d530c6aa5494fd1a61257ad0cf81b3
Arun Nampally <arun.nampally@invitae.com>
2021-12-22 16:21:38 -0800

    use pylsp instead of lsp-python-ms
    - minor change regarding TLS

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/") t)
 (unless package--initialized (package-initialize))
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
