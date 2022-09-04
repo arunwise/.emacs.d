b291a5b4e54fa39de5d901ad86cd1aec836a7367
Arun Nampally <anampally@cs.stonybrook.edu>
2022-09-04 14:25:31 -0700

    cleanup emacs-init.org
    - make org mode more aligned to GTD
    - other minor changes

--- init.el
+++ init.el
@@ -3,6 +3,12 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-archive-priorities
+      '(("melpa-stable" . 20)
+	("gnu" . 10)
+	("melp" . 0)))
 (unless package--initialized (package-initialize))
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
